1a18dc9f66c8bcf8e8fce4ba9a86f471dc9ecc89
Jemma-MS <jemma.m_s@outlook.com>
2023-06-04 23:28:50 -0600

    modified:   update mutations

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -8,9 +8,7 @@
 export const setEntries = ( state, entries ) => {
 
     state.entries = [ ...state.entries, ...entries ]
-    state.isLoading = false
-
-
+    state.isLoading = false 
 }
 
 export const updateEntry = ( state, entry ) => { // entry actualizada
