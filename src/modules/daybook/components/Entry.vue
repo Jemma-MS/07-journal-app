0e035c1597c799f37370fe3d8327fe47f00a50e4
Jemma M S <jemma.m_s@outlook.com>
2024-04-03 21:48:27 -0600

    modified:   src/modules/daybook/components/Entry.vue 	new file:   tests/unit/modules/daybook/routers/index.spec.js

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -18,7 +18,7 @@
 
 <script>
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'Octuber', 'November', 'December']
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 const days   = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 export default {
